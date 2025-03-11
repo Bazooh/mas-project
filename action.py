747503ad933dcdea9e0436ddd431de5c33c88383
Aymeric Conti <aymeric.conti2002@gmail.com>
2025-03-11 15:51:46 +0100

    all abc actions

--- action.py
+++ action.py
@@ -1,14 +1,46 @@
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from utils import Direction
+
+if TYPE_CHECKING:
+    from model import RobotMission
 
 
 class Action(ABC):
     @abstractmethod
-    def apply(self) -> None: ...
+    def apply(self, model: "RobotMission") -> None: ...
+
+    @abstractmethod
+    def can_apply(self, model: "RobotMission") -> bool: ...
 
 
 class Move(Action):
-    def apply(self) -> None: ...
+    def __init__(self, direction: Direction) -> None:
+        self.direction = direction
+
+    def apply(self, model: "RobotMission") -> None: ...
+
+    def can_apply(self, model: "RobotMission") -> bool: ...
 
     @classmethod
     def random(cls) -> "Action":
-        return cls()
+        return cls(Direction.random())
+
+
+class Pick(Action):
+    def apply(self, model: "RobotMission") -> None: ...
+
+    def can_apply(self, model: "RobotMission") -> bool: ...
+
+
+class Drop(Action):
+    def apply(self, model: "RobotMission") -> None: ...
+
+    def can_apply(self, model: "RobotMission") -> bool: ...
+
+
+class Merge(Action):
+    def apply(self, model: "RobotMission") -> None: ...
+
+    def can_apply(self, model: "RobotMission") -> bool: ...
