4db3331a9b458c583c27ee5fbc48c78611e7c687
Aymeric Conti <aymeric.conti2002@gmail.com>
2025-03-11 18:00:41 +0100

    small refactor

--- objects.py
+++ objects.py
@@ -7,18 +7,16 @@ Date: 11/03/2025
 import mesa
 from typing import TYPE_CHECKING
 import random
-from utils import Color
+from utils import Color, Position
 
 if TYPE_CHECKING:
     from .model import RobotMission
 
 
-
 class Radioactivity(mesa.Agent):
+    model: "RobotMission"  # type: ignore
 
-
-    model: "RobotMission" # type: ignore
-    def __init__(self, model: "RobotMission", color: Color, seed=1, x=0, y=0):
+    def __init__(self, model: "RobotMission", color: Color):
         """initialize a Radioactivity instance.
 
         Args:
@@ -27,19 +25,27 @@ class Radioactivity(mesa.Agent):
         """
         super().__init__(model)
         self.color = color
-        self.seed = seed
-        self.x = x
-        self.y = y
-
-        random.seed(self.seed)
 
         self.level = 0
         self.instanciate_level()
-        
+
     def instanciate_level(self):
         if self.color == Color.GREEN:
-            self.level = random.uniform(0, 1/3)
+            self.level = random.uniform(0, 1 / 3)
         elif self.color == Color.YELLOW:
-            self.level = random.uniform(1/3, 2/3)
+            self.level = random.uniform(1 / 3, 2 / 3)
         elif self.color == Color.RED:
-            self.level = random.uniform(2/3, 1)
+            self.level = random.uniform(2 / 3, 1)
+
+
+class Waste(mesa.Agent):
+    model: "RobotMission"  # type: ignore
+
+    def __init__(self, model: "RobotMission", pos: Position):
+        """initialize a Waste instance.
+
+        Args:
+            model: A model instance
+        """
+        super().__init__(model)
+        self.pos = pos
