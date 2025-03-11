087157262b90da3378f8680546b1cc203eb9f2ba
Aymeric Conti <aymeric.conti2002@gmail.com>
2025-03-11 15:31:42 +0100

    agent

--- agent.py
+++ agent.py
@@ -3,3 +3,42 @@ Group: 14
 Members: Aymeric Conti, Pierre Jourdin
 Date: 11/03/2025
 """
+
+import mesa
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from knowledge import Knowledge
+
+if TYPE_CHECKING:
+    from model import RobotMission, Action
+
+
+class Agent(mesa.Agent, ABC):
+    """An agent with fixed initial wealth."""
+
+    model: "RobotMission"  # type: ignore
+
+    def __init__(self, model: "RobotMission"):
+        """initialize a MoneyAgent instance.
+
+        Args:
+            model: A model instance
+        """
+        super().__init__(model)
+        self.knowledge = Knowledge()
+        self.perception = {}
+
+    def step(self) -> None:
+        self.knowledge.update(self.perception)
+        action = self.deliberate()
+        self.perception = self.model.do(action)
+
+    @abstractmethod
+    def deliberate(self) -> Action: ...
+
+
+class RandomAgent(Agent):
+    def deliberate(self) -> Action:
+        return Action.random()
