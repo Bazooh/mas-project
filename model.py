5d6e34833e4dd7c28b285f491efd5c5323c18e62
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2025-03-11 15:51:26 +0100

    model v1

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import mesa
 from agent import RandomAgent as GreenAgent
 from agent import RandomAgent as YellowAgent
 from agent import RandomAgent as RedAgent
-
+import random
 from objects import Radioactivity
 from utils import Color
 
@@ -35,6 +35,7 @@ class RobotMission(mesa.Model):
             seed (int): The seed for the random number generator.
         """
         super().__init__(seed=seed)
+        random.seed(seed)
         self.width = width
         self.height = height
         self.n_green_agents = n_green_agents
@@ -50,12 +51,46 @@ class RobotMission(mesa.Model):
         # yellow region is from green_yellow_border inclusive to yellow_red_border exclusive
         # red region is from yellow_red_border inclusive to width exclusive
 
+        self.place_radioactivity()
+
+        self.place_agents()
+
     def place_radioactivity(self):
         # place green radioactivity
         for i in range(0, self.green_yellow_border):
             for j in range(0, self.height):
                 self.grid.place_agent(Radioactivity(self, Color.GREEN, x=i, y=j), (i, j))
 
+        # place yellow radioactivity
+        for i in range(self.green_yellow_border, self.yellow_red_border):
+            for j in range(0, self.height):
+                self.grid.place_agent(Radioactivity(self, Color.YELLOW, x=i, y=j), (i, j))
+
+        # place red radioactivity
+        for i in range(self.yellow_red_border, self.width):
+            for j in range(0, self.height):
+                self.grid.place_agent(Radioactivity(self, Color.RED, x=i, y=j), (i, j))
+
+    def place_agents(self):
+        # place green agents
+        for agent_idx in range(self.n_green_agents):
+            x = random.randint(0, self.green_yellow_border - 1)
+            y = random.randint(0, self.height - 1)
+            self.grid.place_agent(GreenAgent(self, x, y), (x, y))
+
+        # place yellow agents
+        for agent_idx in range(self.n_yellow_agents):
+            x = random.randint(self.green_yellow_border, self.yellow_red_border - 1)
+            y = random.randint(0, self.height - 1)
+            self.grid.place_agent(YellowAgent(self, x, y), (x, y))
+
+        # place red agents
+        for agent_idx in range(self.n_red_agents):
+            x = random.randint(self.yellow_red_border, self.width - 1)
+            y = random.randint(0, self.height - 1)
+            self.grid.place_agent(RedAgent(self, x, y), (x, y))
+
+    
 
     def step(self):
         """do one step of the model"""
